fa841a52006e0f4c79b61ed87150db53403f6f46
Onekamardin <kamardin199626@mail.ru>
2024-05-23 01:38:33 +0300

    created a horizontalPager

--- app/src/main/java/com/example/learningcard/features/Screen1.kt
+++ app/src/main/java/com/example/learningcard/features/Screen1.kt
@@ -1,5 +1,6 @@
 package com.example.learningcard.features
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,32 +10,42 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.learningcard.ui.theme.BlueMain
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalFoundationApi::class)
 @Preview(showBackground = true)
 @Composable
 fun Screen1() {
 
+    val pagerState = rememberPagerState(pageCount = { 10 })
+    val coroutineScope = rememberCoroutineScope()
+
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
             .background(BlueMain)
     ) {
-        LazyRow(
+        HorizontalPager(
+            state = pagerState,
+            pageSize = PageSize.Fill,
             contentPadding = PaddingValues(top = 150.dp, start = 50.dp, end = 50.dp)
-        ) {
-            items(10) {
-                UiWordItem()
-            }
+        ) { page ->
+            UiWordItem()
         }
         Row(
             modifier = Modifier.fillMaxWidth(),
@@ -42,7 +53,11 @@ fun Screen1() {
         ) {
             IconButton(
                 modifier = Modifier.padding(start = 35.dp),
-                onClick = { }) {
+                onClick = {
+                    coroutineScope.launch {
+                        pagerState.animateScrollToPage(pagerState.currentPage - 1)
+                    }
+                }) {
                 Icon(
                     Icons.Default.ArrowBack,
                     contentDescription = "word_back",
@@ -51,7 +66,11 @@ fun Screen1() {
             }
             IconButton(
                 modifier = Modifier.padding(end = 35.dp),
-                onClick = { }) {
+                onClick = {
+                    coroutineScope.launch {
+                         pagerState.animateScrollToPage(pagerState.currentPage + 1)
+                    }
+                }) {
                 Icon(
                     Icons.Default.PlayArrow,
                     contentDescription = "word_next",
@@ -61,3 +80,4 @@ fun Screen1() {
         }
     }
 }
+
