2536176141dc423903665e61cdd2f201aa520894
Onekamardin <kamardin199626@mail.ru>
2024-08-14 13:59:52 +0300

    update

--- app/src/main/java/com/example/learningcard/features/screen1/Screen1ViewModel.kt
+++ app/src/main/java/com/example/learningcard/features/screen1/Screen1ViewModel.kt
@@ -2,14 +2,8 @@ package com.example.learningcard.features.screen1
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.learningcard.data.model.Dict
-import com.example.learningcard.data.model.WordItem
-import com.example.learningcard.data.model.result.ErrorState
-import com.example.learningcard.data.model.result.ResultState
 import com.example.learningcard.data.repository.Repository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
